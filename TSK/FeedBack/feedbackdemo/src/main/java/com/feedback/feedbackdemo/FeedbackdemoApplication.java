e8aab326dcc8aa4bdfaf64325a7b0e354b620fc6
ramya <ramya@gmail.com>
2024-08-28 17:47:51 +0530

    changes saved

---
+++ TSK/FeedBack/feedbackdemo/src/main/java/com/feedback/feedbackdemo/FeedbackdemoApplication.java
@@ -0,0 +1,13 @@
+package com.feedback.feedbackdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackdemoApplication.class, args);
+	}
+
+}
---
+++ TSK/FeedBack/feedbackdemo/src/test/java/com/feedback/feedbackdemo/FeedbackdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feedback.feedbackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
